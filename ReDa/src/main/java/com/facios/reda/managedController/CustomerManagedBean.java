4616b969fb3e56138e2e7e8f5db0dd5ae3bc426f
jvfacio <jvfacio@jvfacio-PC>
2016-05-23 15:31:27 -0500

    Configuracion de la aplicacion ReDa con los frameworks : * Spring * hibernate * JSF Primefaces

---
+++ ReDa/src/main/java/com/facios/reda/managedController/CustomerManagedBean.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.facios.reda.managedController;
+
+
+import com.facios.reda.spring.model.Customer;
+import com.facios.reda.spring.service.CustomerService;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import org.springframework.dao.DataAccessException;
+
+/**
+ *
+ * @author jvfacio
+ */
+@ManagedBean(name="customerMB")
+@RequestScoped
+public class CustomerManagedBean implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private static final String SUCCESS = "Success";
+    private static final String ERROR   = "error";
+
+    //Spring Customer Service is injected...
+    @ManagedProperty(value="#{CustomerService}")
+    CustomerService customerService;
+
+    List<Customer> customerList;
+
+    private int id;
+    private String name;
+    private String surname;
+
+    /**
+     * Add Customer
+     *
+     * @return String - Response Message
+     */
+    public String addCustomer() {
+        try {
+            Customer customer = new Customer();
+            customer.setId(getId());
+            customer.setName(getName());
+            customer.setSurname(getSurname());
+            getCustomerService().addCustomer(customer);
+            return SUCCESS;
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+
+        return ERROR;
+    }
+
+    /**
+     * Reset Fields
+     *
+     */
+    public void reset() {
+        this.setId(0);
+        this.setName("");
+        this.setSurname("");
+    }
+
+    /**
+     * Get Customer List
+     *
+     * @return List - Customer List
+     */
+    public List<Customer> getCustomerList() {
+        customerList = new ArrayList<Customer>();
+        customerList.addAll(getCustomerService().getCustomers());
+        return customerList;
+    }
+
+    /**
+     * Get Customer Service
+     *
+     * @return ICustomerService - Customer Service
+     */
+    public CustomerService getCustomerService() {
+        return customerService;
+    }
+
+    /**
+     * Set Customer Service
+     *
+     * @param customerService ICustomerService - Customer Service
+     */
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    /**
+     * Set Customer List
+     *
+     * @param customerList List - Customer List
+     */
+    public void setCustomerList(List<Customer> customerList) {
+        this.customerList = customerList;
+    }
+
+    /**
+     * Get Customer Id
+     *
+     * @return int - Customer Id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Set Customer Id
+     *
+     * @param id int - Customer Id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Get Customer Name
+     *
+     * @return String - Customer Name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set Customer Name
+     *
+     * @param name String - Customer Name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get Customer Surname
+     *
+     * @return String - Customer Surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * Set Customer Surname
+     *
+     * @param surname String - Customer Surname
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
